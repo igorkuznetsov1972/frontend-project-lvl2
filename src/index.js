db5ff33f57f1f5f684efb57067d765ca3107e30c
Igor <ikk@mail.ru>
2020-08-06 19:30:35 +0300

    added argument and format option description

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const program = commander.program;
 const newCommand = new commander.Command();
 program
 .version('0.0.1')
-.description('An application for generating diff file');
+.description('Compares two configuration files and shows a difference.')
+.arguments('<filepath1> <filepath2>')
+.option('-f, --format', 'output format');
 program.parse(process.argv);
 console.log(program.args);
 export default program;
