0d03c103ad69a5c48a43b0d6e50c94ddb3f35a4f
Igor <ikk@mail.ru>
2020-08-13 21:40:20 +0300

    flat jsons compare functionality -console output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
-import program from '../src/index.js';
-program();
+import commander from 'commander';
+// eslint-disable-next-line import/extensions
+import gendiffCore from '../src/index.js';
+
+const { program } = commander;
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format', 'output format')
+  .action((filepath1, filepath2) => { gendiffCore(filepath1, filepath2); });
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import commander from 'commander';
-const program = commander.program;
-const newCommand = new commander.Command();
-program
-.version('0.0.1')
-.description('Compares two configuration files and shows a difference.')
-.arguments('<filepath1> <filepath2>')
-.option('-f, --format', 'output format');
-program.parse(process.argv);
-console.log(program.args);
-export default program;
+/* eslint-disable no-console */
+import fs from 'fs';
+import _ from 'lodash';
+
+const readJSON = (filepath) => JSON.parse(fs.readFileSync(filepath));
+const gendiffCore = (filepath1, filepath2) => {
+  const file1 = readJSON(filepath1);
+  const file2 = readJSON(filepath2);
+  const result = {};
+  _.forOwn(file1, (value, key) => {
+    if (file1[key] === file2[key]) {
+      result[`  ${key}`] = file1[key];
+    } else if (!_.has(file2, key)) {
+      result[`- ${key}`] = file1[key];
+    } else {
+      result[`+ ${key}`] = file2[key];
+      result[`- ${key}`] = file1[key];
+    }
+  });
+  _.forOwn(file2, (value, key) => {
+    if (!_.has(file1, key)) {
+      result[`+ ${key}`] = file2[key];
+    }
+  });
+  const diffFile = _.toPairs(result).map((x) => x.join(': '));
+  console.log(diffFile.join('\n'));
+};
+export default gendiffCore;
