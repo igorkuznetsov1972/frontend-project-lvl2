098d39700fae6ee4838e7959bc02c6d2a0bae425
Igor <ikk@mail.ru>
2020-08-14 18:00:08 +0300

    test  created

---
+++ __tests__/index.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-underscore-dangle */
+// eslint-disable-next-line import/extensions
+import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+import path from 'path';
+import fs from 'fs';
+import { test, expect } from '@jest/globals';
+import gendiffCore from '../src/index';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+test('JSON files difference', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const result = readFile('result.txt');
+  expect(gendiffCore(filepath1, filepath2)).toEqual(result);
+});
