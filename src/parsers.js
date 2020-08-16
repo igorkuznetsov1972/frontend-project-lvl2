bd9ad1540e770046c16f353560290d5b68b59910
Igor <ikk@mail.ru>
2020-08-16 10:41:57 +0300

    added ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,18 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 import path from 'path';
 
 export default (filepath) => {
   if (fs.existsSync(filepath)) {
     if (path.extname(filepath) === '.json') {
-      return JSON.parse(fs.readFileSync(filepath));
+      return JSON.parse(fs.readFileSync(filepath, 'UTF-8'));
     }
     if (path.extname(filepath) === '.yaml' || path.extname(filepath) === '.yml') {
-      return yaml.safeLoad(fs.readFileSync(filepath));
+      return yaml.safeLoad(fs.readFileSync(filepath, 'UTF-8'));
+    }
+    if (path.extname(filepath) === '.ini') {
+      return ini.parse(fs.readFileSync(filepath, 'UTF-8'));
     }
     throw new Error('Invalid file type');
   }
