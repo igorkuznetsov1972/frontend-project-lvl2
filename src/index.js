4e768b4db913960766c6d220ec7bd33126e968b9
Igor <ikk@mail.ru>
2020-09-15 10:15:10 +0300

    added formatter choice and default

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
-// import fs from 'fs';
-import _ from 'lodash';
 import parseFile from './parsers.js';
 import genAST from './genAST.js';
-import stylishFormatter from './stylish.js';
+import stylish from './stylish.js';
 
-const gendiffCore = (filepath1, filepath2) => {
+const gendiffCore = (filepath1, filepath2, formatter = 'stylish') => {
   const file1 = parseFile(filepath1);
   const file2 = parseFile(filepath2);
   const result = genAST(file1, file2);
-  console.log(stylishFormatter(result));
+  switch (formatter) {
+    case 'stylish':
+      console.log(stylish(result));
+      return (stylish(result));
+    default:
+      throw new Error('Invalid formatter type');
+  }
 };
 export default gendiffCore;
