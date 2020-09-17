709b2ac3370fcc1d877f0d90497e87fb15ed0c64
Igor <ikk@mail.ru>
2020-09-17 17:25:11 +0300

    updated index.js location

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,8 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
 import { test, expect } from '@jest/globals';
-import gendiffCore from '../src/index';
+// eslint-disable-next-line import/extensions
+import gendiffCore from '../formatters/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
