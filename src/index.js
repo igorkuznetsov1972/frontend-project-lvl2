a3416deaaa9ab81638c3b972514ab3ac6bf1e39b
Igor <ikk@mail.ru>
2020-09-21 11:42:02 +0300

    side effects moved to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 /* eslint-disable import/extensions */
 /* eslint-disable no-console */
+import fs from 'fs';
+import path from 'path';
 import parseFile from './parsers.js';
 import genAST from './genAST.js';
 import formatter from '../formatters/index.js';
 
 const gendiffCore = (filePath1, filePath2, formatterName) => {
-  const fileContent1 = parseFile(filePath1);
-  const fileContent2 = parseFile(filePath2);
-  const result = genAST(fileContent1, fileContent2);
-  return formatter(result, formatterName);
+  if (fs.existsSync(filePath1) && fs.existsSync(filePath2)) {
+    const fileExtention1 = path.extname(filePath1);
+    const fileExtention2 = path.extname(filePath2);
+    const fileContent1 = parseFile(fs.readFileSync(filePath1, 'UTF-8'), fileExtention1);
+    const fileContent2 = parseFile(fs.readFileSync(filePath2, 'UTF-8'), fileExtention2);
+    const result = genAST(fileContent1, fileContent2);
+    return formatter(result, formatterName);
+  }
+  throw new Error('No such file or directory');
 };
 export default gendiffCore;
--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,15 @@
-import fs from 'fs';
 import yaml from 'js-yaml';
 import ini from '@ghostff/ini_parser';
-import path from 'path';
 
-export default (filePath) => {
-  if (fs.existsSync(filePath)) {
-    if (path.extname(filePath) === '.json') {
-      return JSON.parse(fs.readFileSync(filePath, 'UTF-8'));
-    }
-    if (path.extname(filePath) === '.yaml' || path.extname(filePath) === '.yml') {
-      return yaml.safeLoad(fs.readFileSync(filePath, 'UTF-8'));
-    }
-    if (path.extname(filePath) === '.ini') {
-      return ini.parseSync(filePath, true, true);
-    }
-    throw new Error('Invalid file type');
+export default (fileContent, fileExtention) => {
+  if (fileExtention === '.json') {
+    return JSON.parse(fileContent);
   }
-  throw new Error('No such file or directory');
+  if (fileExtention === '.yaml' || fileExtention === '.yml') {
+    return yaml.safeLoad(fileContent);
+  }
+  if (fileExtention === '.ini') {
+    return ini.parseString(fileContent, true, true);
+  }
+  throw new Error('Invalid file type');
 };
