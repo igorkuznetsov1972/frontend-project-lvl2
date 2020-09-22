f1bfe57419c6bfe38fda40e1a257c0392b327fe1
Igor <ikk@mail.ru>
2020-09-22 10:06:16 +0300

    corrected naming to camelcase

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,32 +11,32 @@ import gendiffCore from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const getFixturePath = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
+const readFile = (fileName) => fs.readFileSync(getFixturePath(fileName), 'utf-8');
 describe.each([
   ['JSON', 'nestedFile1.json', 'nestedFile2.json', 'nestedResult.txt'],
   ['YAML', 'file1.yaml', 'file2.yaml', 'result.txt'],
   ['INI', 'file1.ini', 'file2.ini', 'result.txt'],
   ['JSON', 'nestedFile1.json', 'nestedFile2.json', 'plainResult.txt', 'plain'],
   ['JSON', 'nestedFile1.json', 'nestedFile2.json', 'jsonResult.txt', 'json'],
-])('Difference', (name, file1, file2, expected, format) => {
+])('Difference', (name, fileName1, fileName2, expected, format) => {
   test(`${name} files, ${format || 'stylish'} output format`, () => {
-    const filepath1 = getFixturePath(file1);
-    const filepath2 = getFixturePath(file2);
+    const filePath1 = getFixturePath(fileName1);
+    const filePath2 = getFixturePath(fileName2);
     const result = readFile(expected);
-    expect(gendiffCore(filepath1, filepath2, format)).toEqual(result);
+    expect(gendiffCore(filePath1, filePath2, format)).toEqual(result);
   });
 });
 describe.each([
   ['Wrong file type', 'nestedFile1.txt', 'nestedFile2.txt', 'Invalid file type'],
   ['No such file', 'NotSuchNestedFile1.no', 'NotSuchNestedFile2.no', 'No such file or directory'],
   ['Invalid formatter type', 'nestedFile1.json', 'nestedFile2.json', 'Invalid formatter type', 'other'],
-])('Arguments', (name, file1, file2, error, format) => {
+])('Arguments', (name, fileName1, fileName2, error, format) => {
   test(`${name}`, () => {
-    const filepath1 = getFixturePath(file1);
-    const filepath2 = getFixturePath(file2);
+    const filePath1 = getFixturePath(fileName1);
+    const filePath2 = getFixturePath(fileName2);
     expect(() => {
-      gendiffCore(filepath1, filepath2, format);
+      gendiffCore(filePath1, filePath2, format);
     }).toThrow(`${error}`);
   });
 });
