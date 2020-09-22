8ef245e603a061127389fcbe83a874042ccd2510
Igor <ikk@mail.ru>
2020-09-22 09:06:22 +0300

    updated output format choice description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filePath1> <filePath2>')
-  .option('-f, --format [formatter]', 'output format [formatter]', 'stylish')
+  .option('-f, --format [formatter]', 'output format [stylish, plain or json]', 'stylish')
   .action((filePath1, filePath2) => {
     // eslint-disable-next-line no-console
     console.log(gendiffCore(filePath1, filePath2, program.format));
