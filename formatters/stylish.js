d76fc4063e2be0aabe062ebd596a7adf430b74d4
Igor <ikk@mail.ru>
2020-09-23 12:18:13 +0300

    removed magic numbers and reduced cyclomatic complexity

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -2,81 +2,71 @@ import _ from 'lodash';
 
 export default (ast) => {
   const result = ['{\n'];
-  const build = (arr, depth = 4) => {
-    if (Array.isArray(arr)) {
-      const childDepth = depth;
-      if (_.has(arr[1], 'children')) {
-        result.push(`${' '.repeat(childDepth)}${arr[0]}: {\n`);
-        _.toPairs(arr[1].children).sort().forEach((child) => {
-          build(child, childDepth + 4);
-        });
-        result.push(`${' '.repeat(childDepth)}}\n`);
-      }
-      if (!_.has(arr[1], 'modificationType') && !_.has(arr[1], 'children')) {
-        if (!_.isPlainObject(arr[1])) {
-          result.push(`${arr[0]}: ${arr[1]}\n`);
-        }
-        if (_.isPlainObject(arr[1])) {
-          if (childDepth < 12) {
-            result.push(`${' '.repeat(childDepth)}${arr[0]}: {\n${' '.repeat(childDepth + 4)}`);
+  const build = (arr, depth) => {
+    const childDepth = depth;
+    if (!Array.isArray(arr)) result.push(`${arr}`);
+    else if (_.has(arr[1], 'children') && arr[1].type === 'unchanged') {
+      result.push(`${' '.repeat(childDepth)}${arr[0]}: {\n`);
+      _.toPairs(arr[1].children).sort().forEach((child) => {
+        build(child, childDepth + 4);
+      });
+      result.push(`${' '.repeat(childDepth)}}\n`);
+    } else if (_.has(arr[1], 'type')) {
+      switch (arr[1].type) {
+        case 'changed':
+          if (_.isPlainObject(arr[1].beforeValue)) {
+            result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: {\n`);
+            _.toPairs(arr[1].beforeValue).forEach((child) => build(child, childDepth + 4));
+            result.push(`${' '.repeat(childDepth)}}\n`);
           } else {
-            result.push(`${' '.repeat(childDepth - 12)}${arr[0]}: {\n${' '.repeat(childDepth + 4)}`);
-          }
-          _.toPairs(arr[1]).sort().forEach((child) => {
-            build(child, childDepth + 4);
-          });
-          result.push(`${' '.repeat(childDepth)}}\n`);
-        }
-      }
-      if (_.has(arr[1], 'modificationType')) {
-        switch (arr[1].modificationType) {
-          case 'changed':
-            if (_.isPlainObject(arr[1].beforeValue)) {
-              result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: {\n${' '.repeat(childDepth + 4)}`);
-              build(_.toPairs(arr[1].beforeValue, childDepth).flat());
-              result.push(`${' '.repeat(childDepth)}}\n`);
-            } else {
-              result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: ${arr[1].beforeValue}`);
-              result.push('\n');
-            }
-            if (_.isPlainObject(arr[1].afterValue)) {
-              result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: {\n${' '.repeat(childDepth + 4)}`);
-              build(_.toPairs(arr[1].afterValue, childDepth).flat());
-              result.push(`${' '.repeat(childDepth)}}\n`);
-            } else {
-              result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: ${arr[1].afterValue}`);
-              result.push('\n');
-            }
-            break;
-          case 'removed':
-            if (_.isPlainObject(arr[1].beforeValue)) {
-              result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: {\n${' '.repeat(childDepth + 4)}`);
-              _.toPairs(arr[1].beforeValue).forEach((child) => build(child, childDepth + 4));
-              result.push(`${' '.repeat(childDepth)}}\n`);
-            } else {
-              result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: ${arr[1].beforeValue}`);
-              result.push('\n');
-            }
-            break;
-          case 'added':
-            if (_.isPlainObject(arr[1].afterValue)) {
-              result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: {\n${' '.repeat(childDepth + 4)}`);
-              _.toPairs(arr[1].afterValue).forEach((child) => build(child, childDepth + 4));
-              result.push(`${' '.repeat(childDepth)}}\n`);
-            } else {
-              result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: ${arr[1].afterValue}`);
-              result.push('\n');
-            }
-            break;
-          default:
-            result.push(`${' '.repeat(childDepth)}${arr[0]}: `);
-            build(arr[1].value);
+            result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: ${arr[1].beforeValue}`);
             result.push('\n');
-            break;
-        }
+          }
+          if (_.isPlainObject(arr[1].afterValue)) {
+            result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: {\n`);
+            _.toPairs(arr[1].afterValue).forEach((child) => build(child, childDepth + 4));
+            result.push(`${' '.repeat(childDepth)}}\n`);
+          } else {
+            result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: ${arr[1].afterValue}`);
+            result.push('\n');
+          }
+          break;
+        case 'removed':
+          if (_.isPlainObject(arr[1].beforeValue)) {
+            result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: {\n`);
+            _.toPairs(arr[1].beforeValue).forEach((child) => build(child, childDepth + 4));
+            result.push(`${' '.repeat(childDepth)}}\n`);
+          } else {
+            result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: ${arr[1].beforeValue}`);
+            result.push('\n');
+          }
+          break;
+        case 'added':
+          if (_.isPlainObject(arr[1].afterValue)) {
+            result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: {\n`);
+            _.toPairs(arr[1].afterValue).forEach((child) => build(child, childDepth + 4));
+            result.push(`${' '.repeat(childDepth)}}\n`);
+          } else {
+            result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: ${arr[1].afterValue}`);
+            result.push('\n');
+          }
+          break;
+        case 'unchanged':
+          result.push(`${' '.repeat(childDepth)}${arr[0]}: `);
+          build(arr[1].value, childDepth);
+          result.push('\n');
+          break;
+        default:
+          break;
       }
-    } else {
-      result.push(`${arr}`);
+    } else if (!_.isPlainObject(arr[1])) {
+      result.push(`${' '.repeat(childDepth)}${arr[0]}: ${arr[1]}\n`);
+    } else if (_.isPlainObject(arr[1])) {
+      result.push(`${' '.repeat(childDepth)}${arr[0]}: {\n`);
+      _.toPairs(arr[1]).sort().forEach((child) => {
+        build(child, childDepth + 4);
+      });
+      result.push(`${' '.repeat(childDepth)}}\n`);
     }
     return result;
   };
