1873f44a27c363a1c094b97b1734d82b47568ac5
Igor <ikk@mail.ru>
2020-09-23 15:11:56 +0300

    addded subroutines for added and removed strings

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -3,6 +3,26 @@ import _ from 'lodash';
 export default (ast) => {
   const result = ['{\n'];
   const build = (arr, depth) => {
+    const buildRemovedString = (name, value, passedDepth) => {
+      if (_.isPlainObject(value)) {
+        result.push(`${' '.repeat(passedDepth - 2)}- ${name}: {\n`);
+        _.toPairs(value).forEach((child) => build(child, depth + 4));
+        result.push(`${' '.repeat(passedDepth)}}\n`);
+      } else {
+        result.push(`${' '.repeat(passedDepth - 2)}- ${name}: ${value}`);
+        result.push('\n');
+      }
+    };
+    const buildAddedString = (name, value, passedDepth) => {
+      if (_.isPlainObject(value)) {
+        result.push(`${' '.repeat(passedDepth - 2)}+ ${name}: {\n`);
+        _.toPairs(value).forEach((child) => build(child, depth + 4));
+        result.push(`${' '.repeat(passedDepth)}}\n`);
+      } else {
+        result.push(`${' '.repeat(passedDepth - 2)}+ ${name}: ${value}`);
+        result.push('\n');
+      }
+    };
     const childDepth = depth;
     if (!Array.isArray(arr)) result.push(`${arr}`);
     else if (_.has(arr[1], 'children') && arr[1].type === 'unchanged') {
@@ -14,42 +34,14 @@ export default (ast) => {
     } else if (_.has(arr[1], 'type')) {
       switch (arr[1].type) {
         case 'changed':
-          if (_.isPlainObject(arr[1].beforeValue)) {
-            result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: {\n`);
-            _.toPairs(arr[1].beforeValue).forEach((child) => build(child, childDepth + 4));
-            result.push(`${' '.repeat(childDepth)}}\n`);
-          } else {
-            result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: ${arr[1].beforeValue}`);
-            result.push('\n');
-          }
-          if (_.isPlainObject(arr[1].afterValue)) {
-            result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: {\n`);
-            _.toPairs(arr[1].afterValue).forEach((child) => build(child, childDepth + 4));
-            result.push(`${' '.repeat(childDepth)}}\n`);
-          } else {
-            result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: ${arr[1].afterValue}`);
-            result.push('\n');
-          }
+          buildRemovedString(arr[0], arr[1].beforeValue, childDepth);
+          buildAddedString(arr[0], arr[1].afterValue, childDepth);
           break;
         case 'removed':
-          if (_.isPlainObject(arr[1].beforeValue)) {
-            result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: {\n`);
-            _.toPairs(arr[1].beforeValue).forEach((child) => build(child, childDepth + 4));
-            result.push(`${' '.repeat(childDepth)}}\n`);
-          } else {
-            result.push(`${' '.repeat(childDepth - 2)}- ${arr[0]}: ${arr[1].beforeValue}`);
-            result.push('\n');
-          }
+          buildRemovedString(arr[0], arr[1].beforeValue, childDepth);
           break;
         case 'added':
-          if (_.isPlainObject(arr[1].afterValue)) {
-            result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: {\n`);
-            _.toPairs(arr[1].afterValue).forEach((child) => build(child, childDepth + 4));
-            result.push(`${' '.repeat(childDepth)}}\n`);
-          } else {
-            result.push(`${' '.repeat(childDepth - 2)}+ ${arr[0]}: ${arr[1].afterValue}`);
-            result.push('\n');
-          }
+          buildAddedString(arr[0], arr[1].afterValue, childDepth);
           break;
         case 'unchanged':
           result.push(`${' '.repeat(childDepth)}${arr[0]}: `);
