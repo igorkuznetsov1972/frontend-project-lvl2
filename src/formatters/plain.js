62aa5709bf3baeec91610ad2469c063f4be7d638
Igor <ikk@mail.ru>
2020-10-01 17:56:49 +0300

    subroutined repeating operations

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,13 +11,16 @@ export default (ast) => {
       });
     }
     if (!_.has(arr[1], 'type')) return;
+    const buildString = (value, status, eol) => {
+      if (_.isPlainObject(value)) {
+        result.push(`${acc.slice(0, acc.length).join('')}' ${status} [complex value]${eol}`);
+      } else if (value === false || value === true) {
+        result.push(`${acc.slice(0, acc.length).join('')}' ${status} ${value}${eol}`);
+      } else result.push(`${acc.slice(0, acc.length).join('')}' ${status} '${value}'${eol}`);
+    };
     switch (arr[1].type) {
       case 'changed':
-        if (_.isPlainObject(arr[1].beforeValue)) {
-          result.push(`${acc.slice(0, acc.length).join('')}' was updated. From [complex value] to `);
-        } else if (arr[1].beforeValue === false || arr[1].beforeValue === true) {
-          result.push(`${acc.slice(0, acc.length).join('')}' was updated. From ${arr[1].beforeValue} to `);
-        } else result.push(`${acc.slice(0, acc.length).join('')}' was updated. From '${arr[1].beforeValue}' to `);
+        buildString(arr[1].beforeValue, 'was updated. From', ' to ');
         if (_.isPlainObject(arr[1].afterValue)) {
           result.push('[complex value]\n');
         } else if (arr[1].afterValue === false || arr[1].afterValue === true) {
@@ -28,11 +31,7 @@ export default (ast) => {
         result.push(`${acc.join('')}' was removed\n`);
         break;
       case 'added':
-        if (_.isPlainObject(arr[1].afterValue)) {
-          result.push(`${acc.join('')}' was added with value: [complex value]\n`);
-        } else if (arr[1].afterValue === false || arr[1].afterValue === true) {
-          result.push(`${acc.join('')}' was added with value: ${arr[1].afterValue}\n`);
-        } else result.push(`${acc.join('')}' was added with value: '${arr[1].afterValue}'\n`);
+        buildString(arr[1].afterValue, 'was added with value:', '\n');
         break;
       default:
         break;
