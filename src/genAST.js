5ec198093c3f9692fe73687415bf3f68b4047460
Igor <ikk@mail.ru>
2020-10-07 16:41:06 +0300

    improved variables naming

--- src/genAST.js
+++ src/genAST.js
@@ -1,20 +1,20 @@
 import _ from 'lodash';
 
-const genAST = (fileContent1, fileContent2) => {
-  const uniqueKeys = _.union(_.keys(fileContent1), _.keys(fileContent2));
+const genAST = (obj1, obj2) => {
+  const uniqueKeys = _.union(_.keys(obj1), _.keys(obj2));
   const sortedUniqueKeys = _.sortBy(uniqueKeys);
   const ast = sortedUniqueKeys.map((key) => {
-    if (!_.has(fileContent1, key)) {
-      return { name: key, type: 'added', afterValue: fileContent2[key] };
-    } if (!_.has(fileContent2, key)) {
-      return { name: key, type: 'removed', beforeValue: fileContent1[key] };
-    } if (_.isPlainObject(fileContent1[key]) && _.isPlainObject(fileContent2[key])) {
-      return { name: key, type: 'nested', children: (genAST(fileContent1[key], fileContent2[key])) };
-    } if (!_.isEqual(fileContent1[key], fileContent2[key])) {
+    if (!_.has(obj1, key)) {
+      return { name: key, type: 'added', afterValue: obj2[key] };
+    } if (!_.has(obj2, key)) {
+      return { name: key, type: 'removed', beforeValue: obj1[key] };
+    } if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      return { name: key, type: 'nested', children: (genAST(obj1[key], obj2[key])) };
+    } if (!_.isEqual(obj1[key], obj2[key])) {
       return {
-        name: key, type: 'changed', beforeValue: fileContent1[key], afterValue: fileContent2[key],
+        name: key, type: 'changed', beforeValue: obj1[key], afterValue: obj2[key],
       };
-    } return { name: key, type: 'unchanged', value: fileContent1[key] };
+    } return { name: key, type: 'unchanged', value: obj1[key] };
   });
   return ast;
 };
