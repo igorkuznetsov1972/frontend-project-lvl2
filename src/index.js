9182e83e1032d341eb2ac250b9ddc3e5e48e3d60
Igor <ikk@mail.ru>
2020-10-21 16:56:41 +0300

    naming improved

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import fs from 'fs';
 import path from 'path';
-import parseInputData from './parsers.js';
+import parse from './parsers.js';
 import genAST from './genAST.js';
 import format from './formatters/index.js';
 
 const gendiff = (filePath1, filePath2, formatterName) => {
   const fileExtention1 = path.extname(filePath1);
   const fileExtention2 = path.extname(filePath2);
-  const fileContent1 = parseInputData(fs.readFileSync(filePath1, 'UTF-8'), fileExtention1);
-  const fileContent2 = parseInputData(fs.readFileSync(filePath2, 'UTF-8'), fileExtention2);
+  const fileContent1 = parse(fs.readFileSync(filePath1, 'UTF-8'), fileExtention1);
+  const fileContent2 = parse(fs.readFileSync(filePath2, 'UTF-8'), fileExtention2);
   const ast = genAST(fileContent1, fileContent2);
   return format(ast, formatterName);
 };
