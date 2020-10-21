1535893b1a65f76862439d5dfe961dcf2749d38e
Igor <ikk@mail.ru>
2020-10-21 16:49:48 +0300

    refactored stylish  in a more functional style

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,63 +1,33 @@
 import _ from 'lodash';
 
 export default (ast, childIndentation = 4, typeMarkerIndentation = 2) => {
-  const result = ['{\n'];
-  const buildStylishOutput = (node, depth) => {
-    const buildString = (name, value, passedDepth, typeMarker = ' ') => {
-      const buildParamName = () => result.push(`${' '.repeat(passedDepth - typeMarkerIndentation)}${typeMarker} ${name}: {\n`);
-      const closingCurlyBrace = () => result.push(`${' '.repeat(passedDepth)}}\n`);
-      if (_.isArray(value)) {
-        buildParamName();
-        _.forEach(value, (child) => buildStylishOutput(child, passedDepth + childIndentation));
-        closingCurlyBrace();
-      } else if (_.isPlainObject(value)) {
-        buildParamName();
-        _.forEach(_.toPairs(value), (child) => {
-          buildStylishOutput(child, passedDepth + childIndentation);
-        });
-        closingCurlyBrace();
-      } else {
-        result.push(`${' '.repeat(passedDepth - typeMarkerIndentation)}${typeMarker} ${name}: ${value}\n`);
-      }
+  const buildStylishOutput = (arr, depth) => arr.flatMap((node) => {
+    const {
+      name, value, children, type, beforeValue, afterValue,
+    } = node;
+
+    const buildString = (paramName, paramValue, passedDepth, typeMarker = ' ') => {
+      const buildParamName = `${' '.repeat(passedDepth - typeMarkerIndentation)}${typeMarker} ${paramName}: {\n`;
+      const closingCurlyBrace = `${' '.repeat(passedDepth)}}\n`;
+      if (_.isPlainObject(paramValue)) {
+        return `${buildParamName}${Object.entries(paramValue).flatMap(([key, nestedValue]) => buildString(key, nestedValue, passedDepth + childIndentation)).join('')}${closingCurlyBrace}`;
+      } return `${' '.repeat(passedDepth - typeMarkerIndentation)}${typeMarker} ${paramName}: ${paramValue}\n`;
     };
-    const childDepth = depth;
-    if (_.has(node, 'type')) {
-      switch (node.type) {
-        case 'nested':
-          buildString(node.name, node.children, childDepth);
-          break;
-        case 'changed':
-          buildString(node.name, node.beforeValue, childDepth, '-');
-          buildString(node.name, node.afterValue, childDepth, '+');
-          break;
-        case 'unchanged':
-          buildString(node.name, node.value, childDepth);
-          break;
-        case 'added':
-          buildString(node.name, node.afterValue, childDepth, '+');
-          break;
-        case 'removed':
-          buildString(node.name, node.beforeValue, childDepth, '-');
-          break;
-        default:
-          break;
-      }
-    } else if (_.isPlainObject(node)) {
-      _.forEach(_.toPairs(node), (child) => {
-        buildString(child[0], child[1], childDepth);
-      });
-    } else if (!_.isPlainObject(node[1])) {
-      buildString(node[0], node[1], childDepth);
-    } else if (_.isPlainObject(node[1])) {
-      result.push(`${' '.repeat(childDepth)}${node[0]}: {\n`);
-      _.forEach(_.toPairs(node[1]), (child) => {
-        buildString(child[0], child[1], childDepth + childIndentation);
-      });
-      result.push(`${' '.repeat(childDepth)}}\n`);
+
+    switch (type) {
+      case 'nested':
+        return `${' '.repeat(depth)}${name}: {\n${buildStylishOutput(children, depth + childIndentation).join('')}${' '.repeat(depth)}}\n`;
+      case 'changed':
+        return `${buildString(name, beforeValue, depth, '-')}${buildString(name, afterValue, depth, '+')}`;
+      case 'unchanged':
+        return `${buildString(name, value, depth)}`;
+      case 'added':
+        return `${buildString(name, afterValue, depth, '+')}`;
+      case 'removed':
+        return `${buildString(name, beforeValue, depth, '-')}`;
+      default:
+        return new Error(`${type} is not a valid node type`);
     }
-    return result;
-  };
-  _.forEach(ast, (obj) => buildStylishOutput(obj, childIndentation));
-  result.push('}');
-  return result.join('');
+  });
+  return `{\n${buildStylishOutput(ast, childIndentation).join('').trimEnd()}\n}`;
 };
