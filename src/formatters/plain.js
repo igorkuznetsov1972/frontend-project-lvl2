32a4712c6b7033a6c04152b3de6c6ebf3b5e36b7
Igor <ikk@mail.ru>
2020-10-21 18:19:46 +0300

    improved naming

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const buildString = (value) => {
+const stringifyValue = (value) => {
   if (_.isPlainObject(value)) return '[complex value]';
   return _.isString(value) ? `'${value}'` : `${value}`;
 };
@@ -17,11 +17,11 @@ export default (ast) => {
       case 'nested':
         return buildPlainOutput(children, ancestryPath);
       case 'changed':
-        return `Property '${ancestryPath}' was updated. From ${buildString(beforeValue)} to ${buildString(afterValue)}\n`;
+        return `Property '${ancestryPath}' was updated. From ${stringifyValue(beforeValue)} to ${stringifyValue(afterValue)}\n`;
       case 'removed':
         return `Property '${ancestryPath}' was removed\n`;
       case 'added':
-        return `Property '${ancestryPath}' was added with value: ${buildString(afterValue)}\n`;
+        return `Property '${ancestryPath}' was added with value: ${stringifyValue(afterValue)}\n`;
       default:
         return new Error(`${type} is not a valid node type`);
     }
