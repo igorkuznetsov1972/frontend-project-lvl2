b0262bbebf6b289d05e91a1ccf84613ff03bc7b8
Igor <ikk@mail.ru>
2020-11-01 11:43:54 +0300

    Make indentation building depend from recursion depth

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,32 +2,34 @@ import _ from 'lodash';
 
 export default (ast) => {
   const stringify = (arr, depth) => arr.map((node) => {
+    const indent = 4;
     const {
       name, value, children, type, beforeValue, afterValue,
     } = node;
-    const buildIndentation = (passedDepth) => `${' '.repeat(passedDepth)}`;
+    const buildIndentation = (passedDepth) => `${' '.repeat(indent * passedDepth)}`;
+    const buildIndentationForChangedValues = (passedDepth) => `${' '.repeat((indent * passedDepth) - 2)}`;
     const buildString = (paramValue, passedDepth) => {
       const closingCurlyBrace = `${buildIndentation(passedDepth)}}`;
       if (_.isPlainObject(paramValue)) {
-        return _.flattenDeep(['{', Object.entries(paramValue).map(([key, nestedValue]) => [`${buildIndentation(passedDepth + 4)}${key}: ${buildString(nestedValue, passedDepth + 4)}`]), `${closingCurlyBrace}`]).join('\n');
+        return _.flattenDeep(['{', Object.entries(paramValue).map(([key, nestedValue]) => [`${buildIndentation(passedDepth + 1)}${key}: ${buildString(nestedValue, passedDepth + 1)}`]), `${closingCurlyBrace}`]).join('\n');
       } return paramValue;
     };
 
     switch (type) {
       case 'nested':
-        return [`${buildIndentation(depth)}${name}: {`, stringify(children, depth + 4), `${' '.repeat(depth)}}`];
+        return [`${buildIndentation(depth)}${name}: {`, stringify(children, depth + 1), `${buildIndentation(depth)}}`];
       case 'changed':
-        return [`${buildIndentation(depth - 2)}- ${name}: ${buildString(beforeValue, depth)}`, `${buildIndentation(depth - 2)}+ ${name}: ${buildString(afterValue, depth)}`];
+        return [`${buildIndentationForChangedValues(depth)}- ${name}: ${buildString(beforeValue, depth)}`, `${buildIndentationForChangedValues(depth)}+ ${name}: ${buildString(afterValue, depth)}`];
       case 'unchanged':
         return `${buildIndentation(depth)}${name}: ${buildString(value, depth)}`;
       case 'added':
-        return `${buildIndentation(depth - 2)}+ ${name}: ${buildString(afterValue, depth)}`;
+        return `${buildIndentationForChangedValues(depth)}+ ${name}: ${buildString(afterValue, depth)}`;
       case 'removed':
-        return `${buildIndentation(depth - 2)}- ${name}: ${buildString(beforeValue, depth)}`;
+        return `${buildIndentationForChangedValues(depth)}- ${name}: ${buildString(beforeValue, depth)}`;
       default:
         return new Error(`${type} is not a valid node type`);
     }
   });
 
-  return `{\n${_.flattenDeep(stringify(ast, 4)).join('\n')}\n}`;
+  return `{\n${_.flattenDeep(stringify(ast, 1)).join('\n')}\n}`;
 };
