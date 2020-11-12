08196616b76f0fea55ca39c408d6b9a89795a14b
Igor <ikk@mail.ru>
2020-11-12 11:00:33 +0300

    Create entry point in root directiry

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-import fs from 'fs';
-import path from 'path';
-import parse from './src/parsers.js';
-import genAST from './src/genAST.js';
-import format from './src/formatters/index.js';
+import gendiff from './src/index.js';
 
-const gendiff = (filePath1, filePath2, formatterName) => {
-  const fileExtention1 = path.extname(filePath1);
-  const fileExtention2 = path.extname(filePath2);
-  const fileContent1 = parse(fs.readFileSync(filePath1, 'UTF-8'), fileExtention1);
-  const fileContent2 = parse(fs.readFileSync(filePath2, 'UTF-8'), fileExtention2);
-  const ast = genAST(fileContent1, fileContent2);
-  return format(ast, formatterName);
-};
 export default gendiff;
